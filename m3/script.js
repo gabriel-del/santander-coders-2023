f581d510da388a720f33a8ce1d10ce7d5b0d1526
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-13 17:00:59 -0300

    show total

--- m3/script.js
+++ m3/script.js
@@ -1,10 +1,19 @@
-import db from './db.json' assert {type: 'json'};
+import db from './db.json' assert {type: 'json'}
+const main = document.querySelector('main')
+const total = document.querySelector('#total span')
 
-let main = document.querySelector('main')
 db.produtos.forEach(({nome, preco, url}) => main.innerHTML += `
 <div>
   <img src="${url}"></img>
   <h1>${nome}</h1>
   <h2>${preco}</h2>
+  <label>Quantidade:
+  <input type="number" placeholder="0" min="0" max="100" title="0-99"/>
 </div>`)
-console.log(db.produtos)
+
+document.querySelectorAll('input').forEach(item => item.addEventListener('blur', () => {
+  let acc = 0
+  document.querySelectorAll('main > div').forEach(item => {
+    acc += parseInt(item.querySelector('h2').innerText) * parseInt(item.querySelector('input').value || 0)})
+  total.innerHTML = acc
+}))
