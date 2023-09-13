d497e0bf7c2af160f29bd3e3b6443a4289582d87
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-13 14:53:38 -0300

    add products on db.json and html

--- m3/script.js
+++ m3/script.js
@@ -1,3 +1,10 @@
 import db from './db.json' assert {type: 'json'};
 
+let main = document.querySelector('main')
+db.produtos.forEach(({nome, preco, url}) => main.innerHTML += `
+<div>
+  <img src="${url}"></img>
+  <h1>${nome}</h1>
+  <h2>${preco}</h2>
+</div>`)
 console.log(db.produtos)
