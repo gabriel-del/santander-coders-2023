5bfa144c611ad5751e2bbdbe409da25fd0fcb745
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-09-19 22:34:02 -0300

    working

--- m4/1calculadora/script.js
+++ m4/1calculadora/script.js
@@ -1,7 +1,18 @@
 
 const input = document.querySelector('input')
+const result = document.querySelectorAll('input')[1]
 
 
 document.querySelectorAll('button').forEach(button => 
-  button.addEventListener('click', () => input.value += button.innerText)
-  )
+  button.addEventListener('click', () => {
+    if(/\*|-|\+|\/|\./g.test(button.innerText) && input.value.length === 0) return
+    if (button.innerText === 'Del') {
+      input.value = input.value.slice(0,-1)
+    } else if (button.innerText === 'AC') { 
+      input.value = ''
+    } else {
+      input.value += button.innerText
+    }
+    if(Number(button.innerText)) result.value = eval(input.value)  
+  })
+)
