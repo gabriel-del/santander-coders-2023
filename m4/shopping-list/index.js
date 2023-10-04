27541f28e1930402790a5321e3a4fa0717829afd
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-04 04:44:32 -0300

    reduce setData

--- m4/shopping-list/index.js
+++ m4/shopping-list/index.js
@@ -1,39 +1,18 @@
 const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0d3hidHdzZmd6amdyZXFhd2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxNDU2OTgsImV4cCI6MjAxMTcyMTY5OH0.SYOZEtkpU8sdd8dqIhJLaRZfOG5J50WB-khWUkO2I6w"
 
 const apiUrl = 'https://xtwxbtwsfgzjgreqawla.supabase.co/rest/v1/products'
+const header = {"Content-Type": "application/json", apikey: apiKey, authorization: `Bearer ${apiKey}` }
 
 async function getData() {
-  return await (await fetch(apiUrl, { method: "GET", headers: {apikey: apiKey, authorization: `Bearer ${apiKey}` }})).json()
+  return await (await fetch(apiUrl, { method: "GET", headers: header})).json()
+}
+async function setData(data) {
+  return (await fetch(apiUrl, { method: "POST", headers: header, body: JSON.stringify(data)})).status
 }
 
 btnGet.addEventListener("click", async () => console.log(await getData()))
-btnSet.addEventListener("click", async () => console.log(await setData()))
-
-
-
-
-async function setData(dado) {
-  const res = await fetch(
-    'https://xtwxbtwsfgzjgreqawla.supabase.co/rest/v1/products',
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Prefer: "return=minimal",
-        apikey: apiKey,
-        authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify(dado),
-    }
-  )
-  return 'foi'
-}
-
-let dado = {
-  id: 8,
-  name: "bolacha",
-  price: 500,
-  created_at: '2023-10-03T00:56:25.802884+00:00'
-}
-
+btnSet.addEventListener("click", async () => console.log(await setData({
+  name: document.querySelector('#inputName').value,
+  price: document.querySelector('#inputPrice').value
+})))
 
