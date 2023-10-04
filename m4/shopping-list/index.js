e9a3a3fb7f34570de46706aa4b3407611373ed62
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-04 05:49:37 -0300

    working with arrow functions

--- m4/shopping-list/index.js
+++ m4/shopping-list/index.js
@@ -4,13 +4,8 @@ const supabaseUrl = 'https://xtwxbtwsfgzjgreqawla.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0d3hidHdzZmd6amdyZXFhd2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxNDU2OTgsImV4cCI6MjAxMTcyMTY5OH0.SYOZEtkpU8sdd8dqIhJLaRZfOG5J50WB-khWUkO2I6w'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function getData(){
-return (await supabase.from('products').select('*')).data
-}
-
-async function setData(data){
-return (await supabase.from('products').insert(data)).status
-}
+const getData = async () =>  (await supabase.from('products').select('*')).data
+const setData = async data =>  (await supabase.from('products').insert(data)).status
 
 btnGet.addEventListener("click", async () => console.log(await getData()))
 btnSet.addEventListener("click", async () => console.log(await setData({
