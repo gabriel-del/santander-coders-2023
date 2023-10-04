bea4252d4747ae3249462fed829fc1b1ca21bbce
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-04 05:43:57 -0300

    remove old set-get Data

--- m4/shopping-list/index.js
+++ m4/shopping-list/index.js
@@ -1,26 +1,19 @@
-async function getData() {
-  return await (await fetch(apiUrl, { method: "GET", headers: header})).json()
-}
-async function setData(data) {
-  return (await fetch(apiUrl, { method: "POST", headers: header, body: JSON.stringify(data)})).status
-}
-
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 const supabaseUrl = 'https://xtwxbtwsfgzjgreqawla.supabase.co'
 // const supabaseKey = process.env.SUPABASE_KEY
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0d3hidHdzZmd6amdyZXFhd2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxNDU2OTgsImV4cCI6MjAxMTcyMTY5OH0.SYOZEtkpU8sdd8dqIhJLaRZfOG5J50WB-khWUkO2I6w'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function getData2(){
+async function getData(){
 return (await supabase.from('products').select('*')).data
 }
 
-async function setData2(data){
+async function setData(data){
 return (await supabase.from('products').insert(data)).status
 }
 
-btnGet.addEventListener("click", async () => console.log(await getData2()))
-btnSet.addEventListener("click", async () => console.log(await setData2({
+btnGet.addEventListener("click", async () => console.log(await getData()))
+btnSet.addEventListener("click", async () => console.log(await setData({
   name: document.querySelector('#inputName').value,
   price: document.querySelector('#inputPrice').value
 })))
