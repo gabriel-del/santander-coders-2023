bf6114bb63b21a9f5ac4fa2d4f19a2b614ce9946
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-04 05:38:08 -0300

    remove coments

--- m4/shopping-list/index.js
+++ m4/shopping-list/index.js
@@ -1,8 +1,3 @@
-const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0d3hidHdzZmd6amdyZXFhd2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxNDU2OTgsImV4cCI6MjAxMTcyMTY5OH0.SYOZEtkpU8sdd8dqIhJLaRZfOG5J50WB-khWUkO2I6w"
-
-const apiUrl = 'https://xtwxbtwsfgzjgreqawla.supabase.co/rest/v1/products'
-const header = {"Content-Type": "application/json", apikey: apiKey, authorization: `Bearer ${apiKey}` }
-
 async function getData() {
   return await (await fetch(apiUrl, { method: "GET", headers: header})).json()
 }
@@ -10,16 +5,10 @@ async function setData(data) {
   return (await fetch(apiUrl, { method: "POST", headers: header, body: JSON.stringify(data)})).status
 }
 
-
-
-// import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-
-// const supabase = createClient('https://xtwxbtwsfgzjgreqawla.supabase.co', 'public-anon-key')
-const supabase = createClient('https://xtwxbtwsfgzjgreqawla.supabase.co', apiKey)
-// const supabaseKey = process.env.SUPABASE_KEY
-
+const supabaseUrl = 'https://xtwxbtwsfgzjgreqawla.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0d3hidHdzZmd6amdyZXFhd2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxNDU2OTgsImV4cCI6MjAxMTcyMTY5OH0.SYOZEtkpU8sdd8dqIhJLaRZfOG5J50WB-khWUkO2I6w'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function getData2(){
 return (await supabase.from('products').select('*')).data
