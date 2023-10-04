b88e882d3e02c3927461c22ec4d275443dcfe81b
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-04 05:30:20 -0300

    getData2 working

--- m4/shopping-list/index.js
+++ m4/shopping-list/index.js
@@ -10,9 +10,27 @@ async function setData(data) {
   return (await fetch(apiUrl, { method: "POST", headers: header, body: JSON.stringify(data)})).status
 }
 
-btnGet.addEventListener("click", async () => console.log(await getData()))
+
+
+// import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+
+// const supabase = createClient('https://xtwxbtwsfgzjgreqawla.supabase.co', 'public-anon-key')
+const supabase = createClient('https://xtwxbtwsfgzjgreqawla.supabase.co', apiKey)
+// const supabaseKey = process.env.SUPABASE_KEY
+
+
+async function getData2(){
+let { data: products, error } = await supabase.from('products').select('*')
+return products
+}
+
+btnGet.addEventListener("click", async () => console.log(await getData2()))
 btnSet.addEventListener("click", async () => console.log(await setData({
   name: document.querySelector('#inputName').value,
   price: document.querySelector('#inputPrice').value
 })))
+
+
 
