9692ceac7dd9e19e77a9fef06ac3d7fddb485e1f
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-27 14:22:22 -0300

    add eslint.config.js

---
+++ m5/eslint.config.js
@@ -0,0 +1,19 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {rules:
+    {
+      // 'no-console': 'off',
+      // 'ts/space-infix-ops': 'off',
+      // 'style/object-curly-spacing': ['error', 'never'],
+      // 'max-statements-per-line': ['error', {max: 6}],
+      // 'ts/brace-style': ['error', '1tbs', {allowSingleLine: true}],
+      // 'arrow-parens': ['error', 'as-needed'],
+      // 'antfu/if-newline': 0,
+      // 'block-spacing': ['error', 'never'],
+      // 'promise/param-names': 'off',
+      // 'no-new': 0,
+      // 'style/lines-between-class-members': ['error', 'never'],
+    },
+  },
+)
