37cf622c94d393a4ee1eeb847c682636c28ae55c
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-26 21:50:05 -0300

    reduce app.component

--- m5/src/app/app.component.ts
+++ m5/src/app/app.component.ts
@@ -3,9 +3,48 @@ import { Task } from 'src/models/task.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-})
+  template: `<div class="container pb-5">
+  <!-- <app-task-form (addTask)="onAddTask($event)"></app-task-form> -->
+  <app-task-form-reactive
+    (addTask)="onAddTask($event)"
+  ></app-task-form-reactive>
+
+  <app-task-list
+    [tasks]="listTask"
+    (handleTask)="handleTask($event)"
+  ></app-task-list>
+
+  <div #myDiv>
+    <div *ngIf="selectedTask">
+      <app-task-detail
+        [task]="selectedTask"
+        (closeDetail)="fecharDetalhes()"
+      ></app-task-detail>
+    </div>
+
+    <div *ngFor="let items of toDoStatus">
+      {{items.title}}
+    </div>
+  </div>
+</div>
+<style>
+  .blue {
+    color: blue;
+}
+
+.red { 
+    color: red;
+}
+
+.bg-green { 
+    background-color: green;
+}
+
+color-red { 
+    color: red; 
+}
+</style>
+`})
 export class AppComponent {
   @ViewChild('myDiv') detailDiv!: ElementRef;
 
