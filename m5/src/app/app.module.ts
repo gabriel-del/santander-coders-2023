9d8f8f2d0dcb62745f213d9decc65b109b08d633
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-26 22:18:51 -0300

    move tasks one level up

--- m5/src/app/app.module.ts
+++ m5/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskFormComponent } from './components/task-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskListComponent } from './components/task-list.component';
 import { CurrencyFormatPipe } from './pipes/currency-format.pipe';
-import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { TaskDetailComponent } from './components/task-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DateGreaterThanTodayDirective } from './directives/date-greater-than-today.directive';
-import { TaskFiltroComponent } from './components/task-filtro/task-filtro.component';
-import { TaskFormReactiveComponent } from './components/task-form-reactive/task-form-reactive.component';
+import { TaskFiltroComponent } from './components/task-filtro.component';
+import { TaskFormReactiveComponent } from './components/task-form-reactive.component';
 import { DateMaskDirective } from './directives/date-mask.directive';
 @NgModule({
   declarations: [
