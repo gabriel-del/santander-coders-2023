c5bf560e027d74d9df6eec6403c8ee740be4bbb9
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-26 22:14:02 -0300

    join task-filtro

--- m5/src/app/components/task-filtro/task-filtro.component.ts
+++ m5/src/app/components/task-filtro/task-filtro.component.ts
@@ -2,8 +2,23 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-task-filtro',
-  templateUrl: './task-filtro.component.html',
-  styleUrls: ['./task-filtro.component.scss'],
+  template: `
+  <label for="filtro">Filtro</label>
+<select
+  name="filtro"
+  id="status"
+  [(ngModel)]="filtro"
+  (change)="handleSelect()"
+>
+  <option value="all">Todos os Status</option>
+  <option value="toDo">ToDo</option>
+  <option value="trabalhando">Trabalhando</option>
+  <option value="finalizado">Finalizado</option>
+</select>
+`,
+  styles: [`.testes { 
+    background: red;
+}`],
 })
 export class TaskFiltroComponent {
   @Output() onChangeFiltro = new EventEmitter();
