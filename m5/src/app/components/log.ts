e13c10755f24939228e9ba7f09cca0d1ecd2770e
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-27 20:16:34 -0300

    reduce log

--- m5/src/app/components/log.ts
+++ m5/src/app/components/log.ts
@@ -26,22 +26,11 @@ import {Task} from 'src/models/task.model'
     h2 { text-align: center; }}
     `],
 })
-export class Log implements OnInit {
+export class Log {
   columns = [
     {name: 'To Do', id: 'toDo'},
     {name: 'In Progress', id: 'trabalhando'},
     {name: 'Done', id: 'finalizado'},
   ]
   @Input() tasks: Task[] = []
-  @Output() handleTask = new EventEmitter()
-  tasksFiltradas: Task[] = []
-  ngOnInit() {this.tasksFiltradas = this.tasks}
-  selectedTask(task: Task) {this.handleTask.emit(task)}
-  handleFiltro(filtro: string) {
-    if (filtro === 'all') {
-      this.tasksFiltradas = this.tasks
-      return
-    }
-    this.tasksFiltradas = this.tasks.filter(item => item.status === filtro)
-  }
 }
