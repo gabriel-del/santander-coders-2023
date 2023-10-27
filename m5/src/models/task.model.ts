695bc683e1944829d3cbec1b6398f26670029619
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-27 14:26:27 -0300

    eslint on models

--- m5/src/models/task.model.ts
+++ m5/src/models/task.model.ts
@@ -1,15 +1,15 @@
 export class Task {
-  title: string;
-  description: string;
-  date: string | Date;
-  status: string;
-  tags?: string[];
+  title: string
+  description: string
+  date: string | Date
+  status: string
+  tags?: string[]
 
   constructor(title = '', description = '', date = '', status = '', tags = []) {
-    this.title = title;
-    this.description = description;
-    this.date = date;
-    this.status = status;
-    this.tags = tags;
+    this.title = title
+    this.description = description
+    this.date = date
+    this.status = status
+    this.tags = tags
   }
 }
