89583102be8b51f737b99a95e2f9c78681cfaeb2
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-26 22:08:32 -0300

    join task-list

--- m5/src/app/components/task-list/task-list.component.ts
+++ m5/src/app/components/task-list/task-list.component.ts
@@ -10,8 +10,50 @@ import { Task } from 'src/models/task.model';
 
 @Component({
   selector: 'app-task-list',
-  templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss'],
+  template: `<div>
+  <h2>Tarefas</h2>
+
+  <app-task-filtro (onChangeFiltro)="handleFiltro($event)"></app-task-filtro>
+
+  <div class="kanban-board mt-3">
+    <div class="column" *ngFor="let column of columns">
+      <h2>{{ column.name }}</h2>
+      <ng-container *ngFor="let item of tasksFiltradas">
+        <ng-container *ngIf="item.status === column.id">
+          <div class="card px-4 pt-2 mb-2">
+            <h4>{{ item.title }}</h4>
+            <p>{{ item.description }}</p>
+
+            <div>
+              <button class="btn btn-light w-100" (click)="selectedTask(item)">
+                Editar
+              </button>
+              <button
+                class="btn btn-info w-100 mt-2 mb-3"
+                (click)="selectedTask(item)"
+              >
+                Ver detalhes
+              </button>
+            </div>
+          </div>
+        </ng-container>
+      </ng-container>
+    </div>
+  </div>
+</div>
+`,
+  styles: [`
+  .kanban-board { display: flex;  flex-direction: row; }
+  .column { 
+    &:nth-child(2) { 
+      margin: 0 8px; } flex: 1; padding: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    background-color: #f0f0f0;
+
+    h2 {
+      text-align: center;
+}}`],
 })
 export class TaskListComponent implements OnInit {
 
