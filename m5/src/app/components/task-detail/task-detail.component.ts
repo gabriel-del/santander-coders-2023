3ca0f616e7e58ecec441a48c9ae754835e777037
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-10-26 22:14:53 -0300

    join task-detail

--- m5/src/app/components/task-detail/task-detail.component.ts
+++ m5/src/app/components/task-detail/task-detail.component.ts
@@ -3,7 +3,26 @@ import { Task } from 'src/models/task.model';
 
 @Component({
   selector: 'app-task-detail',
-  templateUrl: './task-detail.component.html'})
+  template: `<div class="mt-5 border p-3">
+  <div class="d-flex flex-shrink-0 align-items-center justify-content-between">
+    <h2>Detalhes da Tarefa</h2>
+    <button
+    (click)="close()"
+      type="button"
+      class="btn-close"
+      data-bs-dismiss="modal"
+      aria-label="Close"
+    ></button>
+  </div>
+
+  <p><strong>Título:</strong> {{ task.title }}</p>
+  <p><strong>Descrição:</strong> {{ task.description }}</p>
+  <p>
+    <strong>Data de Vencimento:</strong> {{ task.date | date : "dd/MM/yyyy" }}
+  </p>
+  <p><strong>Status:</strong> {{ task.status }}</p>
+</div>
+`})
 export class TaskDetailComponent {
   @Input() task = new Task();
   @Output() closeDetail = new EventEmitter();
